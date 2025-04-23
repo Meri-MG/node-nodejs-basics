2aed89e99624abeb4360cb563c296d2088672f89
Meri-MG <mgogi13@freeuni.edu.ge>
2025-04-23 12:27:46 +0400

    feat: impelement create.js functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs'
+
+const content = 'I am fresh and young'
+const filePath = './src/fs/files/fresh.txt'
+
 const create = async () => {
-    // Write your code here 
-};
+  if (fs.existsSync(filePath)) throw new Error('FS operation failed')
+
+  fs.writeFile(filePath, content, err => {
+    if (err) throw err
+  })
+}
 
 await create();
