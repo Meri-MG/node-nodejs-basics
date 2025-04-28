287445928f4e44f58c9ddf7216c6c5960d5814b6
Meri-MG <mgogi13@freeuni.edu.ge>
2025-04-28 14:03:54 +0400

    feat: impelement copy.js functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,36 @@
+import fs from 'node:fs'
+import path from 'node:path'
+const destination = './src/fs/files-copy/'
+const src = './src/fs/files/'
+
 const copy = async () => {
-    // Write your code here 
-};
+
+    if (!fs.existsSync(src)) {
+      throw new Error('FS operation failed');
+    }
+    
+    if (fs.existsSync(destination)) {
+      throw new Error('FS operation failed');
+    }
+
+    copyFolderSync(src, destination);
+}
+
+function copyFolderSync(source, destination) {
+  fs.mkdirSync(destination, { recursive: true });
+
+  const entries = fs.readdirSync(source, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const srcPath = path.join(source, entry.name);
+    const destPath = path.join(destination, entry.name);
+
+    if (entry.isDirectory()) {
+      copyFolderSync(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
 
 await copy();
