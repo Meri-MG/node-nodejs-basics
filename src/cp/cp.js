21ca0c84146b14c8b8ca6533cd95b253ee9cbf3f
Meri-MG <mgogi13@freeuni.edu.ge>
2025-04-28 16:26:03 +0400

    feat: impelement childprocess functionality

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'child_process';
+
+const spawnChildProcess = (args) => {
+    const child = spawn('node', ['script.js', ...args]);
+
+    process.stdin.pipe(child.stdin);
+
+    child.stdout.pipe(process.stdout);
+
+    child.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
+
+    child.on('error', (err) => {
+        console.error(`Failed to start child process: ${err.message}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
