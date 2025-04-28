ca18b4dec0bd7883d692c06da88b88aeabec2dba
Meri-MG <mgogi13@freeuni.edu.ge>
2025-04-28 16:06:16 +0400

    feat: impelement zlibs functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const inputFilePath = 'src/zip/files/fileToCompress.txt';
+    const outputFilePath = 'src/zip/files/archive.gz';
+  
+    const inputStream = createReadStream(inputFilePath);
+  
+    const outputStream = createWriteStream(outputFilePath);
+  
+    const gzipStream = createGzip();
+  
+    inputStream.pipe(gzipStream).pipe(outputStream);
+  
+    outputStream.on('finish', () => {
+      console.log('File has been compressed successfully to archive.gz');
+    });
+  
+    inputStream.on('error', (err) => {
+      console.error('Error reading the file:', err.message);
+    });
+  
+    outputStream.on('error', (err) => {
+      console.error('Error writing the compressed file:', err.message);
+    });
 };
 
 await compress();
