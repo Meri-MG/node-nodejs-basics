b64410fea116cdaaa8e4b778ea805518e0d5c0ab
Meri-MG <mgogi13@freeuni.edu.ge>
2025-04-28 15:24:13 +0400

    feat: impelement calcHash.mjs functionality

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.resolve('src/hash/files/fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on('end', () => {
+    const result = hash.digest('hex');
+    console.log(result);
+  });
+
+  stream.on('error', (err) => {
+    console.error('Error reading file:', err.message);
+  });
 };
 
 await calculateHash();
