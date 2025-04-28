7395a5faadd6c52af59a99b1fbb7cfce9581ff8e
Meri-MG <mgogi13@freeuni.edu.ge>
2025-04-28 15:52:20 +0400

    feat: impelement strems functionality

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from 'fs';
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream('src/fs/files/fileToWrite.txt', { flags: 'w', encoding: 'utf8' });
+
+  console.log('Please enter some text to write to file:');
+
+  process.stdin.pipe(writeStream);
+
+  process.stdin.on('end', () => {
+    console.log('Input has been written to file successfully.');
+  });
+
+  process.stdin.on('error', (err) => {
+    console.error('Error reading input:', err.message);
+  });
 };
 
 await write();
